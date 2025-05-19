7210e2df10b69cc29ab0e7741cf5f672a8c72a76
JoshDaSneaky <walsh.joshua.aaron@gmail.com>
2025-05-19 14:01:07 +1000

    Implemented basic AI API functionality

--- app/views/default.py
+++ app/views/default.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from app.ai import get_fact
 
 v = Blueprint("default", __name__)
 
@@ -6,3 +7,8 @@ v = Blueprint("default", __name__)
 @v.route("/")
 def index():
     return render_template("pages/index.html.j2")
+
+@v.route("/analytics")
+def show_data():
+    fact = get_fact()
+    return render_template("pages/analytics.html.j2", data=fact)
