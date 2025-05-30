69f17330faeb2a3c7e8ae738146027d004618a0c
James Dagnall <james.t.dagnall@gmail.com>
2025-05-30 15:07:46 +1000

    fixed entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,9 @@
 
 PRODUCT="proto"
 
-# Run monit daemon, which is in charge of ensuring proxysql is running
-# Monit will start proxysql and if it ever dies it will restart it
-# https://mmonit.com/monit/
-# chmod 700 /home/app/prototype/config/monitrc
-# monit -c /home/app/prototype/config/monitrc &
-
-# We use the flask dev server for local development
 echo "Installing python dependencies..."
 
-exec pip install -r requirements.txt
+pip install -r requirements.txt
 
 echo "Starting flask dev server..."
 
